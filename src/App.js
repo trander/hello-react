8e015d1e5bfc784cc8c1b22c72b35b49f97d8bd8
deve <dev.hje@gmail.com>
2018-10-07 01:26:21 +0900

    className

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -14,7 +15,7 @@ class App extends Component {
       msTransition: 'all'
     }
     return (
-      <div>
+      <div className="my-div">
         <h1>Hello React!</h1>
         <h2>{text}</h2>
         {
